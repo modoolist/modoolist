9482c213d51ccf522acf5d9bcd7d27f841ae3a8e
HyeokjinKang <coupy@croissant.tech>
2021-11-03 22:23:00 +0900

    initialize

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { env } from "process";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  plugins: [svelte()],
+  server: {
+    port: env.PORT || 80,
+    hmr: false,
+  },
+});
