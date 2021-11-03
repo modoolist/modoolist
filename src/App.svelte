8a076d5b07c7086ffd7744d61c8042c729f14b9e
HyeokjinKang <coupy@croissant.tech>
2021-11-03 22:04:53 +0900

    Login/Join/Index 구현을 위한 기초 마련

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,16 @@
 <script>
-	export let name;
+  import { Router, Route } from "svelte-routing";
+  import Index from "./routes/Index.svelte";
+  import Login from "./routes/auth/Login.svelte";
+  import Join from "./routes/auth/Join.svelte";
+
+  export let url = "";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
+<Router url="{url}">
+	<!-- / -->
+	<Route path="/"><Index /></Route>
+	<!-- /auth -->
+	<Route path="/auth/login"><Login /></Route>
+	<Route path="/auth/join"><Join /></Route>
+</Router>
---
+++ src/routes/Index.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Link, navigate } from "svelte-routing";
+  if(!localStorage.logined) {
+    navigate("/auth/login", { replace: true });
+  }
+</script>
+
+<app>
+
+</app>
