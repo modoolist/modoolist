00a859a8ca9052fbf626f714123c4a0fbf146992
HyeokjinKang <coupy@croissant.tech>
2021-11-04 22:53:37 +0900

    로그인 완성~

--- src/routes/auth/Login.svelte
+++ src/routes/auth/Login.svelte
@@ -5,6 +5,11 @@
 
 <main>
   <img src="/images/logo.png" alt="Modoo-list">
+  <input type="text" placeholder="아이디" id="username">
+  <input type="text" placeholder="비밀번호" id="password">
+  <button>로그인</button>
+  <span>모두리스트 계정이 없으신가요?</span>
+  <a href="/auth/join">회원가입</a>
 </main>
 
 <style>
@@ -14,10 +19,57 @@
     align-items: center;
     justify-content: center;
     width: 100vw;
-    height: 80vh;
+    height: 90vh;
   }
 
   img {
     width: 40vw;
+  }
+
+  input::placeholder {
+    font-size: 2vh;
+    color: #7B7B7B;
+  }
+
+  input[type=text] {
+    font-size: 2vh;
+    font-weight: 700;
+    padding: 1vh 5vw;
+    width: 70vw;
+    height: 4vh;
+    margin-top: 2vh;
+    border-radius: 1vh;
+    border: 2px solid #ddd;
+    background-color: #FFFFFF;
+  }
+
+  button {
+    font-weight: 800;
+    font-size: 2.5vh;
+    width: 45vw;
+    height: 7vh;
+    margin-top: 5vh;
+    border-radius: 5vh;
+    border: none;
+    background-color: #4B90FF;
+    color: #FFFFFF;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+  }
+
+  #username {
+    margin-top: 10vh;
+  }
+
+  span {
+    font-size: 1.7vh;
+    font-weight: 400;
+    margin-top: 2vh;
+    color: #B4B4B4;
+  }
+
+  a {
+    font-size: 1.7vh;
+    font-weight: 600;
+    color: #555;
   }
 </style>
